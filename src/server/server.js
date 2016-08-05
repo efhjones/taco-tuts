526a977cfc9bd2b43d982383c5202eb00ea8d5c3
Emily Jones <efhjones@gmail.com>
2016-08-05 13:44:41 -0600

    Adds basic express server

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../dist'));
 
 app.listen(3000);
-console.log("potatoes");
 console.log("So many tacos on on 3000");
 
 module.exports = app;
